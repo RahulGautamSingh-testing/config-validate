6a549ba7aea8aea2efb6bf467e74b9474d75d0a3
RahulGautamSingh-testing <99875673+RahulGautamSingh-testing@users.noreply.github.com>
2022-10-26 17:49:45 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  platform: "gitlab",
+  endpoint: "https://gitlab.example.com/api/v4/",
+  token: "gitlab-token",
+  autodiscover: true,
+  gitAuthor: "Renovate Bot <name.name@example.com>",
+  labels: ["label1", "label2", "label3"],
+  onboarding: true,
+  logLevel: "debug",
+  gitfs: process.env.RENOVATE_GIT_FS,
+  onboardingConfig: {
+    extends: ["config:base", ":dependencyDashboardApproval"]
+  },
+  autodiscoverFilter: "one/specific/repo/for/testing",
+  hostRules: [
+    {
+      hostType: "maven",
+      matchHost: "https://artifactory.example.com",
+      username: "artifactory-username",
+      password: "artifactory-password"
+    },
+  ],
+  packageRules: [
+    {
+      matchManagers: ["maven"],
+      registryUrls: [
+        "https://artifactory.example.com/first-repo",
+        "https://artifactory.example.com/second-repo"
+      ],
+      automerge: false,
+      matchUpdateTypes: ["minor", "patch", "pin", "digest"]
+    }
+  ]
+};
